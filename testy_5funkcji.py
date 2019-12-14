3f4419c17d0762f0f1956cf53794cfbcec22c690
agipacz-gh <czapiga.joanna@gmail.com>
2019-12-14 15:39:04 +0100

    dodano 2 funkcje

--- testy_5funkcji.py
+++ testy_5funkcji.py
@@ -1,12 +1,18 @@
 import unittest
 import duze
 
-class testy_kalkulator(unittest.TestCase):
+class testy_5funkcji(unittest.TestCase):
         def test_duze_litery(self):
             wynik = duze.duze_litery('ala ma kota')
             self.assertEqual(wynik, 'Ala ma kota')
 
+            print(wynik)
 
+        def test_split(self):
+            wynik = duze.split_litery('ala ma kota')
+            self.assertEqual(wynik, ['Ala', 'ma', 'kota')
+
+            print(wynik)
 
 if __name__ == '__main__':
     unittest.main()
